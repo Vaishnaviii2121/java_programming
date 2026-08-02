6234651ed076a4b5c263cfdc1cbf51f8bbc7bad6
Vaishnavi Garad <vaishnavibg2504@gmail.com>
2026-08-02 11:26:47 +0530

    Add Addition.java for basic addition functionality

---
+++ programs/Addition.java
@@ -0,0 +1,13 @@
+public class Addition
+{
+    public static void main(String A[])
+    {
+        int a = 11;
+        int b = 10;
+        int ans = 0;
+
+        ans = a + b;
+
+        System.out.println("Addition : "+ans);
+    }
+}
