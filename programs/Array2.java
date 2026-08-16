acc15607c488bf9d75f37f0265714830054968e9
Vaishnavi Garad <vaishnavibg2504@gmail.com>
2026-08-16 08:25:55 +0530

    Add Array2.java to display student marks

---
+++ programs/Array2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Array2 
+{
+    public static void main(String args[])
+    {
+        int[] marks = new int[3];
+
+        marks[0] = 97;
+        marks[1] = 99;
+        marks[2] = 95;
+
+        for(int i = 0; i < 3 ; i++)
+        {
+            System.out.println(marks[i]);
+        }
+    }    
+}
